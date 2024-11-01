9db846c19d8e89255f711e37fe26aa89ba06797a
Quratabid <162566815+Quratabid@users.noreply.github.com>
2024-11-01 17:23:36 +0500

    Add files via upload

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'next/link'
+const Footer = () => {
+  return (
+    <div>
+      <footer className='bg-black w-full mt-24'>
+        <div className='text-white flex justify-center  py-12 px-6'>
+        <Link href="https://www.linkedin.com/in/kiran-abid-1967972ba/" >Made by Kiran Abid</Link>
+        </div>
+
+      </footer>
+ 
+
+    </div>
+  )
+}
+
+export default Footer
